9d6e236b68a44c076177b5b468efe37da253e7f7
chj <bluedog129@gmail.com>
2025-04-16 00:20:24 +0900

    fix: 패딩값 조정

--- src/components/management/Topbar.tsx
+++ src/components/management/Topbar.tsx
@@ -8,7 +8,7 @@ const TopbarContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 15px 10px 15px 20px;
+  padding: 15px 20px 15px 20px;
   background-color: white;
 `;
 
